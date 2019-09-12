4c58f026a51690963d135fedc9025004c774d5f6
Juan1899 <53906952+Juan1899@users.noreply.github.com>
2019-09-12 17:56:40 -0500

    Add files via upload
    Se agregó la clase "Card", que es el tipo de navegación base de nuestra aplicación. La clase "Card" se vinculó con el index.

---
+++ src/js/view/Card.js
@@ -0,0 +1,68 @@
+class Card {
+
+    constructor(posX, posY, image, index){
+      
+        this.posX = posX;
+        this.posY = posY;
+        this.image = image;
+        this.index = index;
+        this.nombre = "";
+        this.precio;
+        this.over = false;
+        this.trash1 = loadImage("data/trashS.png");
+        this.trash2 = loadImage("data/trashL.png");
+        this.plus1 = loadImage("data/plusS.png");
+        this.plus2 = loadImage("data/plusL.png");
+        
+    }
+
+    paint(){
+
+        image(this.image,this.posX,this.posY);
+        textSize(15);
+        textAlign(LEFT);
+        fill(0);
+        text(this.nombre,this.posX+20,this.posY+110);
+        text("$"+ this.precio,this.posX+20,this.posY+135);
+
+        switch(this.index){
+
+        case 0:
+
+        if (mouseX >= this.posX+320 && mouseX <= this.posX + 340
+            && mouseY >= this.posY+120 && mouseY <= this.posY + 150) {
+        
+            image(this.trash2,this.posX+320,this.posY+120);
+            this.over = true;
+        
+        }else {
+
+            image(this.trash1,this.posX+320,this.posY+120);
+            this.over = false;
+        }
+
+        break;
+
+        case 1:
+            
+         if (mouseX >= this.posX+315 && mouseX <= this.posX + 345
+            && mouseY >= this.posY+120 && mouseY <= this.posY + 150) {
+        
+            image(this.plus2,this.posX+315,this.posY+120);
+            this.over = true;
+        
+        }else {
+
+            image(this.plus1,this.posX+315,this.posY+120);
+            this.over = false;
+        }
+
+        break;
+
+        }
+
+    }
+
+
+
+}
